23de72443c1c23ca311cf6c01dc6ba2c03b921d2
jadenmanuel <jadenmanuel2006@gmail.com>
2022-12-04 10:58:04 -0500

    Added GameOver with Text appearing at collision

--- FlappyBird Project/Assets/Scripts/BoundaryCollider.cs
+++ FlappyBird Project/Assets/Scripts/BoundaryCollider.cs
@@ -1,9 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class BoundaryCollider : MonoBehaviour
 {
+    public TextMeshProUGUI gameOverText;
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -21,6 +25,8 @@ public class BoundaryCollider : MonoBehaviour
 
         if (other.gameObject.tag == "Obstacle")
         {
+            gameOverText.gameObject.SetActive(true);
+
             Debug.Log("Over!");
 
             Time.timeScale = 0;
--- FlappyBird Project/Assets/Scripts/GameManager.cs
+++ FlappyBird Project/Assets/Scripts/GameManager.cs
@@ -1,13 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    public TextMeshProUGUI gameOverText;
     // Start is called before the first frame update
     void Start()
     {
-        
+        //gameOverText.gameObject.SetActive(true);
     }
 
     // Update is called once per frame
